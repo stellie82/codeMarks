96d923501d6edbb64603ea4d0e634f448ae574c5
Josh Cosson <joshlcosson@gmail.com>
2020-02-05 18:08:11 -0800

    Updated Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import PostDetail from "./components/PostDetail"
-import Hero from "./components/Hero"
+import PostDetail from "./components/PostDetail";
+import Hero from "./components/Hero";
 
 class App extends Component {
-
   state = {
-    user: false,
+    user: false
   };
 
   componentDidMount() {
@@ -15,59 +14,86 @@ class App extends Component {
   }
 
   checkAuthState() {
-    let authFailureString = 'Unauthenticated.';
-    let authAbnormalString = 'Unexpected authentication state.';
-    let queryString = '/checkAuthState';
+    let authFailureString = "Unauthenticated.";
+    let authAbnormalString = "Unexpected authentication state.";
+    let queryString = "/checkAuthState";
     let queryOptions = {
-      method: 'GET',
-      credentials: 'include',
+      method: "GET",
+      credentials: "include",
       headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
+        Accept: "application/json",
+        "Content-Type": "application/json",
         "Access-Control-Allow-Credentials": true
       }
     };
     fetch(queryString, queryOptions)
       .then(response => {
         switch (response.status) {
-          case 200: return response.json(); break;
-          case 401: throw new Error(authFailureString); break;
-          default: throw new Error(authAbnormalString); break;
+          case 200:
+            return response.json();
+            break;
+          case 401:
+            throw new Error(authFailureString);
+            break;
+          default:
+            throw new Error(authAbnormalString);
+            break;
         }
       })
-      .then(userInfo => this.setState({ user: userInfo }) )
-      .catch(error => this.setState({ user: false }) );
+      .then(userInfo => this.setState({ user: userInfo }))
+      .catch(error => this.setState({ user: false }));
   }
 
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" render={(props) => (
-            <div>
-              <span>codemarks</span>
-              <Hero {...props} />
-            </div>
-          )} />
-          <Route exact path="/login/local">
-            <span>username: [input here]</span> <br/>
-            <span>password: [input here]</span> <br/>
-            <span>password: [input here]</span>
-          </Route>
-          <Route path="/viewpost">
-            <PostDetail />
-          </Route>
-          <Route path="/newpost">
-            <span>post composer</span>
-          </Route>
-          <Route path="/profile">
-            <span>user profile</span>
-          </Route>
+          <Route
+            exact
+            path="/"
+            render={props => (
+              <div>
+                <span>codemarks</span>
+                <Hero {...props} />
+              </div>
+            )}
+          />
+          <Route
+            exact
+            path="/login/local"
+            render={props => <div>Login form here</div>}
+          />
+
+          <Route
+            path="/viewpost"
+            render={props => (
+              <div>
+                <PostDetail {...props} />
+              </div>
+            )}
+          />
+
+          <Route
+            path="/newpost"
+            render={props => (
+              <div>
+                <PostComposer {...props} />
+              </div>
+            )}
+          />
+
+          <Route
+            path="/profile"
+            render={props => (
+              <div>
+                <UserProfile {...props} />
+              </div>
+            )}
+          />
         </Switch>
       </Router>
     );
   }
-
 }
 
 export default App;
