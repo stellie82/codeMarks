d1a620eb7dcf7cf60b71cf11e6a50982ba31c391
Josh Cosson <joshlcosson@gmail.com>
2020-02-09 10:23:05 -0800

    Updated PostsController routers

--- controllers/PostsController.js
+++ controllers/PostsController.js
@@ -2,19 +2,27 @@ const db = require("../models");
 
 // Defining methods for the PostsController
 module.exports = {
+  //find all posts
   findAll: function(req, res) {
     db.Post.find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  //get post details
   findById: function(req, res) {
     db.Post.findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  //create post with associated github user id
   create: function(req, res) {
-    db.Post.create(req.body)
+    db.Post.create({
+      author: req.body.author,
+      title: req.body.title,
+      description: req.body.description,
+      githubid: req.user.social.github.id
+    })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,14 +4,19 @@ const postsController = require("../../controllers/postsController");
 // Matches with "/api/posts"
 router
   .route("/")
+  //find all posts
   .get(postsController.findAll)
+  //create a new post
   .post(postsController.create);
 
 // Matches with "/api/posts/:id"
 router
   .route("/:id")
+  //find post by ID
   .get(postsController.findById)
+  //update post
   .put(postsController.update)
+  //delete post
   .delete(postsController.remove);
 
 module.exports = router;
