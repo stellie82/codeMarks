7e7cc1c6e5d5420d2c597b4a34e069a24248dd71
Josh Cosson <joshlcosson@gmail.com>
2020-02-16 14:25:03 -0800

    setting up heroku for testing

---
+++ client/build/precache-manifest.442edac138d9599244a98df2d4343d52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e312ab15c2b0908f93a5",
+    "url": "/static/js/main.f33a3c76.chunk.js"
+  },
+  {
+    "revision": "476e50d60f1439331c7c",
+    "url": "/static/js/2.24d17a2c.chunk.js"
+  },
+  {
+    "revision": "e312ab15c2b0908f93a5",
+    "url": "/static/css/main.088cac60.chunk.css"
+  },
+  {
+    "revision": "103888406e4c1e01145fd47d5a5fb45f",
+    "url": "/index.html"
+  }
+];
