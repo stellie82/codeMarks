4fadaf13991c1159e2fc2ee5d84bb8f56df809dc
patrickschroedl <patrick.schroedl@gmail.com>
2020-02-17 18:29:22 -0800

    Stateful display options

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -19,14 +19,14 @@ class Header extends Component {
       <div className="header">
         <Link to="/" className="title">codemarks</Link>
         <div className="btn-row markOptions">
-          <span className="rounded-btn icon-btn-before popularMarks" onClick={this.props.handleViewPopularPosts}>Popular marks</span>
-          <span className="rounded-btn icon-btn-before recentMarks" onClick={this.props.handleViewRecentPosts}>Recent marks</span>
-          { this.props.authenticated ? <span to="/mine" className="skyblue rounded-btn icon-btn-before myMarks" onClick={this.handleViewMyPosts}>My marks</span> : '' }
-          { this.props.authenticated ? <Link to="/newpost" className="lime rounded-btn icon-btn-before newMark">New mark</Link> : '' }
+          <span className={`markOption popularMarks ${this.props.displayOptions ? (this.props.displayOptions.popular ? 'selected' : '') : ''}`} onClick={this.props.handleViewPopularPosts}>Popular marks</span>
+          <span className={`markOption recentMarks ${this.props.displayOptions ? (this.props.displayOptions.recent ? 'selected' : '') : ''}`} onClick={this.props.handleViewRecentPosts}>Recent marks</span>
+          { this.props.authenticated ? <span to="/mine" className={`markOption myMarks ${this.props.displayOptions ? (this.props.displayOptions.mine ? 'selected' : '') : ''}`} onClick={this.handleViewMyPosts}>My marks</span> : '' }
+          { this.props.authenticated ? <Link to="/newpost" className="markOption newMark">New mark</Link> : '' }
         </div>
         {this.props.authenticated ?
           (<span className="authStatus">Welcome back, {this.props.user.social.github.username}.
-              <a className="red rounded-btn icon-btn-after signOut" href="http://localhost:3001/auth/logout">Sign out</a>
+              <a className="red rounded-btn icon-btn-before signOut" href="http://localhost:3001/auth/logout">Sign out</a>
            </span>)
             :
           (<span className="authStatus"></span>)
