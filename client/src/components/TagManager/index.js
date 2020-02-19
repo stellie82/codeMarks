5cf608a105728e1b411000a1694aefbe819bbce2
patrickschroedl <patrick.schroedl@gmail.com>
2020-02-19 13:12:01 -0800

    Integrated Josh's TagManager

--- client/src/components/TagManager/index.js
+++ client/src/components/TagManager/index.js
@@ -6,15 +6,29 @@ class TagManager extends Component {
   // TODO: add the mouseenter and mouseleave event hander to pass the "code highlight" events up to the parent PostDetail component
 
   state = {
-    tagData: []
+    tagData: [],
+    tagSearchResults: []
   };
 
   componentDidMount() {
     this.loadTags();
   }
 
+  handleSearchInput = event => {
+    this.setState({ searchInput: event.target.value.split(" ") });
+    setTimeout(this.searchForTags, 200);
+  };
+
   renderTagList() {
     return this.state.tagData.map(tag => <Tag tagData={tag} key={tag._id} />);
+  }
+
+  renderTagSearchResults() {
+    if (!this.state.tagSearchResults || this.state.tagSearchResults.length === 0) {
+      return (<span className="noSearchResults">No results</span>);
+    } else {
+      return this.state.tagSearchResults.map(tag => <Tag tagData={tag} key={tag._id} />);
+    }
   }
 
   loadTags = () => {
@@ -35,17 +49,47 @@ class TagManager extends Component {
       });
   };
 
+  searchForTags = () => {
+    fetch("http://localhost:3001/api/tags/searchTag", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Credentials": true
+      },
+      body: JSON.stringify(this.state.searchInput)
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({ tagSearchResults: data });
+      });
+  };
+
   render() {
     return (
       <div className="tagManager">
         <span className="tagManagerHeader">Selected Tags</span>
-        <div className="filterTags">...</div>
+        <div className="filterTags"></div>
         <span className="tagManagerHeader">Tag Search</span>
-        <div className="tagSearchContainer">...</div>
-        <span className="tagManagerHeader">Popular Tags</span>
-        <div className="popularTags">
-          {this.renderTagList()}
+        <div className="tagSearchContainer">
+          <input
+            type="text"
+            id="tagSearch"
+            className="tagSearch"
+            autocomplete="off"
+            placeholder="Search for tags..."
+            ref={this.tagSearchInput}
+            onChange={this.handleSearchInput}
+          ></input>
+          <div className="searchedTags">
+            {this.renderTagSearchResults()}
+          </div>
         </div>
+        <span className="tagManagerHeader">Popular Tags</span>
+        <div className="popularTags">{this.renderTagList()}</div>
       </div>
     );
   }
