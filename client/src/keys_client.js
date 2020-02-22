b8949b237fcdcd67238f05530463ff1680958ca4
stellie82 <kims82@gmail.com>
2020-02-21 19:50:52 -0800

    update client home page redirect for Gouri

--- client/src/keys_client.js
+++ client/src/keys_client.js
@@ -4,5 +4,5 @@ module.exports = {
   CLIENT_ID: (process.env.NODE_ENV === "production" ? process.env.PROD_CLIENT_ID : process.env.DEV_CLIENT_ID ),
   SECRET_ID: (process.env.NODE_ENV === "production" ? process.env.PROD_CLIENT_SECRET :process.env.DEV_CLIENT_SECRET ),
   APP_DOMAIN: (process.env.NODE_ENV === "production" ? "" : "http://localhost:3001"),  
-  CLIENT_HOME_PAGE_URL : (process.env.NODE_ENV === "production" ? "\"/\"" : "http://localhost:3000"), 
+  CLIENT_HOME_PAGE_URL : (process.env.NODE_ENV === "production" ? "https://codemarks-app.herokuapp.com" : "http://localhost:3000"), 
 };
